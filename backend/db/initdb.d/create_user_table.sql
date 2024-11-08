395b4ad1bef88cfca863c6d318b9fb2806b696f1
Y-Ayaka373 <147482711+Y-Ayaka373@users.noreply.github.com>
2024-11-08 14:00:29 +0900

    Delete backend directory

--- backend/db/initdb.d/create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_information (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    email VARCHAR(128) NOT NULL,
-    password VARCHAR(16) NOT NULL,
-    PRIMARY KEY (id)
-);
--- backend/db/initdb.d/init_user_information_table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_information (name, email, password) VALUES ("tanaka taro", "tanaka.taro@example.com", "pass1");
-INSERT INTO user_information (name, email, password) VALUES ("yamada hanako", "yamada.hanako@example.com", "pass2");
